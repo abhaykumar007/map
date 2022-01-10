61859a0c2f2065143b9a18d50f6bc11623833a11
abhay <abhaybhagat2609@gmail.com>
2022-01-10 11:22:46 +0530

    done

--- src/component/map.js
+++ src/component/map.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-// import { Map } from "react-leaflet";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 function MapCompo() {
+  // co-ordinates of Banglore
   const position = [12.9716, 77.5946];
+
+  // data is geoJson information
   const [data, setData] = useState();
   const [user, setUser] = useState([]);
-  const [updated, setUpdated] = useState([]);
+
+  // sorting data Area-ID
   const [dataByID, setDataByID] = useState();
 
+  // fetching Users
   async function getUser() {
     let ref = [];
     let response = await fetch("https://kyupid-api.vercel.app/api/users");
@@ -15,58 +19,56 @@ function MapCompo() {
     ref.push(waitRes);
     setUser(ref);
   }
-
-  function updateUser() {
-    let ref = [];
-    let info = [];
-    // console.log("ref object", Object.keys(ref));
-
-    user[0]?.users.map((element, i) => {
-      if (
-        Object.keys(ref) == [] ||
-        Object.keys(ref).findIndex((ele) => ele == element.area_id) < 0
-      ) {
-        ref[element.area_id] = [element];
-
-        if (element.gender == "M") {
-          info[element.area_id] = { gender: { male: 1, female: 0 } };
-        } else {
-          info[element.area_id] = { gender: { male: 0, female: 1 } };
-        }
-        if (element.is_pro_user) {
-          info[element.area_id] = { ...info[element.area_id], pro: 1 };
-        } else {
-          info[element.area_id] = { ...info[element.area_id], pro: 2 };
-        }
-        info[element.area_id] = { ...info[element.area_id], user: 1 };
-        info[element.area_id] = {
-          ...info[element.area_id],
-          area_id: element.area_id,
-        };
-      } else {
-        ref[element.area_id].push(element);
-        if (element.gender == "M") {
-          info[element.area_id].gender.male = ++info[element.area_id].gender
-            .male;
-        } else {
-          info[element.area_id].gender.female = ++info[element.area_id].gender
-            .female;
-        }
-        if (element.is_pro_user) {
-          info[element.area_id].pro = ++info[element.area_id].pro;
-        }
-
-        info[element.area_id].user = ++info[element.area_id].user;
-      }
-    });
-    // console.log("OBject array", Object.values(info));
-    setDataByID(Object.values(info));
-    setUpdated(ref);
-  }
+  // fetching geoJson data
   async function getArea() {
     let response = await fetch("https://kyupid-api.vercel.app/api/areas");
     let waitRes = await response.json();
     setData(waitRes);
+  }
+
+  // collecting data by its AreaID
+  function updateUser() {
+    if (user.length > 0) {
+      let info = [];
+
+      user[0]?.users.map((element, i) => {
+        if (
+          Object.keys(info) == [] ||
+          Object.keys(info).findIndex((ele) => ele == element.area_id) < 0
+        ) {
+          if (element.gender == "M") {
+            info[element.area_id] = { gender: { male: 1, female: 0 } };
+          } else {
+            info[element.area_id] = { gender: { male: 0, female: 1 } };
+          }
+          if (element.is_pro_user) {
+            info[element.area_id] = { ...info[element.area_id], pro: 1 };
+          } else {
+            info[element.area_id] = { ...info[element.area_id], pro: 2 };
+          }
+          info[element.area_id] = { ...info[element.area_id], user: 1 };
+          info[element.area_id] = {
+            ...info[element.area_id],
+            area_id: element.area_id,
+          };
+        } else {
+          if (element.gender == "M") {
+            info[element.area_id].gender.male = ++info[element.area_id].gender
+              .male;
+          } else {
+            info[element.area_id].gender.female = ++info[element.area_id].gender
+              .female;
+          }
+          if (element.is_pro_user) {
+            info[element.area_id].pro = ++info[element.area_id].pro;
+          }
+
+          info[element.area_id].user = ++info[element.area_id].user;
+        }
+      });
+      // console.log("OBject array", Object.values(info));
+      setDataByID(Object.values(info));
+    }
   }
 
   useEffect(() => {
@@ -77,64 +79,21 @@ function MapCompo() {
     getUser();
     getArea();
   }, []);
-  // console.log("data", dataByID);
-  // console.log("data", data);
+
+  // map the updated data by POP-UP
   function onEachFeature(feature, layer) {
-    // let result = dataByID.find(
-    //   (element) => element && element.area_id == feature.properties.area_id
-    // );
-
-    // dataByID.forEach((element) => console.log(element, typeof element));
-    // console.log("results", result);
-    // debugger;
-
-    // console.log("feature", typeof feature.properties);
-    let mix = [];
-    console.log("type of databyId", typeof dataByID);
-    if (dataByID && feature.properties) {
-      let ref = [];
-      ref.push(feature.properties);
-      ref.map((element) => {
-        let result = dataByID.find((ele) => ele.area_id == element.area_id);
-        if (result) {
-          element = { ...element, result };
-          mix.push(element);
-        }
-      });
-      console.log("dataUpdatedMIX", mix);
-      if (mix && mix.length > 0 && mix[0].result) {
-        let detalis = `"Area-name": ${mix[0].name},
-        "Area-ID": ${mix[0].area_id},
-        "Area pin-code": ${mix[0].pin_code},
-        "User":${mix[0].result.user},
-        "Pro-User":${mix[0].result.pro},
-        "Male/Female":${mix[0].result.gender.male}/${mix[0].result.gender.female}`;
-        layer.bindPopup(detalis);
-      }
+    let result = dataByID.find(
+      (element) => element.area_id == feature.properties.area_id
+    );
+    if (feature.properties && result) {
+      let detalis = `"Area-name": ${feature.properties.name},
+      "Area-ID": ${feature.properties.area_id},
+      "Area pin-code": ${feature.properties.pin_code},
+      "User":${result?.user},
+      "Pro-User":${result?.pro},
+      "Male/Female":${result?.gender.male}/${result?.gender.female}`;
+      layer.bindPopup(detalis);
     }
-
-    // if (
-    //   feature.properties &&
-    //   feature.properties.name &&
-    //   feature.properties.area_id &&
-    //   feature.properties.pin_code
-    // ) {
-    //   // let result = dataByID.find(
-    //   //   (element) => element.area_id == feature.properties.area_id
-    //   // );
-
-    //   // let detalis = `"Area-name": ${feature.properties.name},
-    //   // "Area-ID": ${feature.properties.area_id},
-    //   // "Area pin-code": ${feature.properties.pin_code},
-    //   // "User":${result?.user},
-    //   // "Pro-User":${result?.pro},
-    //   // "Male/Female":${result?.gender.male}/${result?.gender.female}`;
-
-    //   let detalis = `"Area-name": ${feature.properties.name},
-    //     "Area-ID": ${feature.properties.area_id},
-    //     "Area pin-code": ${feature.properties.pin_code}`;
-    //   layer.bindPopup(detalis);
-    // }
   }
 
   return (
@@ -146,10 +105,6 @@ function MapCompo() {
           zoom={11}
           scrollWheelZoom={false}
         >
-          {/* <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          /> */}
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -161,11 +116,6 @@ function MapCompo() {
             onEachFeature={onEachFeature}
             onclick={(event) => event.stopPropagation()}
           />
-          {/* <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker> */}
         </MapContainer>
       )}
     </div>
@@ -173,5 +123,3 @@ function MapCompo() {
 }
 
 export default MapCompo;
-
-// https://www.youtube.com/watch?v=cCOL7MC4Pl0&t=513s
